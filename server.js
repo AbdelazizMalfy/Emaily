fa3bd2948fd0633f0fa5fa7676940d5afd4f36cd
Abdelaziz <abdelazizMalfy@gmail.com>
2019-09-26 18:58:36 +0200

    initial commit

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys');
 // const bodyParser = require('body-parser');
+
+
+
 const app = express();
 
 // app.use(bodyParser.json())
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}), (accessToken) => {
+    console.log(accessToken)
+})
 
 const PORT = process.env.PORT || 3001;
 
