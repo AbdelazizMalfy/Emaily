ee21882b6c94e5a6ab55ecbd1613e09742ecb70f
Abdelaziz <abdelazizMalfy@gmail.com>
2019-09-26 18:19:04 +0200

    initial commit

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const app = express();
 
 // app.use(bodyParser.json())
 
+// CLIENT_ID 1023944982056-7n8ht52sn7i0n996vg4b14qmbgv5q4tp.apps.googleusercontent.com
 
-app.get('/',(req,res)=>{
-    res.send({hi:'there Everybody'});
-});
+// CLIENT_SECRET _hVITBVRcctJk6WtIPYnvF8i
 
 const PORT = process.env.PORT || 3001;
 
