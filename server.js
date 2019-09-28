1f56b4f0d3570834d2b7f6525edaf96ab70df493
Abdelaziz <abdelazizMalfy@gmail.com>
2019-09-28 18:04:37 +0200

    authentication added

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const keys = require('./config/keys')
+const keys = require('./config/keys');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+
+
+//Models 
+require('./models/user');
+
+//Passport
 require('./services/passport');
-// const bodyParser = require('body-parser');
 
 mongoose.connect(keys.mongoURI)
 
 const app = express();
 
-// app.use(bodyParser.json())
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKeys]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
--- services/passport.js
+++ services/passport.js
@@ -1,13 +1,41 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+
+// Generate Token by encoding user id and cookiekey
+passport.serializeUser((user,done)=>{
+    done(null,user.id)
+})
+
+// Get the user back from decoding the token and the cookiekey
+
+passport.deserializeUser((id,done)=>{
+    User.findById(id)
+        .then((user)=>{
+            done(null,user)
+        })
+})
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile,done) => {
-    console.log(accessToken,"/---------------------",refreshToken,"/---------------", profile)
+    User.findOne({googleId:profile.id})
+        .then( foundUser => {
+            if (foundUser){
+                // Already exist in the DB don't creat it again
+                done(null,foundUser);
+            } else {
+                // Doesn't exist in the DB create a new record
+                const user = new User({googleId : profile.id})
+                user.save().then(user=> done(null,user))
+            }
+        })
 }))
 
 
