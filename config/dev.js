259bd8c16fb1343e77fbc437d4b5ac3eb3645273
Abdelaziz <abdelazizMalfy@gmail.com>
2019-09-28 19:33:58 +0200

    deployment changes

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '547510937793-8cr6dk5e31pi1i4u5gvchem69lmu797e.apps.googleusercontent.com',
+    googleClientSecret: 'Q80dbxCMnGzJuUaXxPdCt_vS',
+    mongoURI:'mongodb+srv://Abdelaziz:test123@cluster0-hwumf.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKeys:'iidonjrjowknekfkmkmsomcasbhfbvjhkgirngnrjene'
+}
